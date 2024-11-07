b2a7a07df1dd7c0b033932ee01a95a73ba339cd0
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-07 16:17:14 +0200

    Refactored Counter class to use Redux store and add subscribe functionality + commented out original code

--- scripts.js
+++ scripts.js
@@ -1,12 +1,46 @@
 class Counter {
-    value = 1;
-    increase() {
-        this.value += 1
+    // Constructor initializes the counter with an initial state
+    constructor() {
+        this.state = { count: 0 };
+        this.listeners = [];
     }
-    decrease() {
-        this.value -= 1
+
+    // Get the current state of the counter
+    getState() {
+        return this.state;
     }
-    reset() {
-        this.value.reset()
+
+    // Subscribe a listener function to be called whenever the state changes
+    subscribe(listener) {
+        this.listeners.push(listener);
     }
 }
+// Create a new counter instance
+const counter = new Counter();
+
+/* { type: 'ADD' }
+{ type: 'SUBTRACT' }
+{ type: 'RESET' } 
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'ADD':
+      return state + 1;
+    case 'SUBTRACT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+let store = Redux.createStore(counter);
+store.subscribe(() => console.log(store.getState()));
+store.dispatch({ type: 'ADD' }); 
+    add() {
+        this.state.value++;
+    }
+    subtract() {
+        this.state.value--;
+    }
+    reset() {
+        this.state.value = 0;
+    }
+ */
