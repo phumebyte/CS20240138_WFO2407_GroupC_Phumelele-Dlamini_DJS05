e46cfea2687708637826d982a4b698f20e8436e7
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-10 12:03:54 +0200

    Added action creators that will change initial state

--- scripts.js
+++ scripts.js
@@ -1,2 +1,7 @@
 /* Initial state */
 const initialState = { count: 0 };
+
+/* Action creators */
+const add = () => ({ type: "ADD" });
+const subtract = () => ({ type: "SUBTRACT" });
+const reset = () => ({ type: "RESET" });
