94f3142bd79351e1de928c5ae1544d7b61d284e0
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-10 12:08:23 +0200

    Wrote reducer function

--- scripts.js
+++ scripts.js
@@ -5,3 +5,18 @@ const initialState = { count: 0 };
 const add = () => ({ type: "ADD" });
 const subtract = () => ({ type: "SUBTRACT" });
 const reset = () => ({ type: "RESET" });
+
+/* Reducer function */
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "ADD":
+        return { count: state.count + 1 };
+      case "SUBTRACT":
+        return { count: state.count - 1 };
+      case "RESET":
+        return { count: 0 };
+      default:
+        return state;
+    }
+};
+  
